da0d49c3e4c7c746e7092606fb0c985ac57a1da4
Ben Nooner <bdnooner@outlook.com>
2024-12-17 00:25:39 -0700

    add UI to rock paper scissors

--- javascript.js
+++ javascript.js
@@ -23,50 +23,124 @@ function getHumanChoice() {
   }
 }
 
-function playGame() {
-  function playRound(human, computer) {
-    let result = human - computer;
-
-    switch (result) {
-      case -2:
-        console.log("You win!");
-        humanScore += 1;
-        break;
-      case -1:
-        console.log("You lose...");
-        compScore += 1;
-        break;
-      case 0:
-        console.log("It was a tie.");
-        break;
-      case 1:
-        console.log("You win!");
-        humanScore += 1;
-        break;
-      case 2:
-        console.log("You lose...");
-        compScore += 1;
-        break;
+function playRound(human, computer) {
+  function getChoiceText(choice) {
+    switch(choice) {
+      case 1: 
+        return "Rock";
+      case 2: 
+        return "Paper";
+      case 3: 
+        return "Scissors";
     }
   }
+  let humanChoice = getChoiceText(human);
+  let computerChoice = getChoiceText(computer);
+  let result = human - computer;
 
-  let humanScore = 0;
-  let compScore = 0;
+  p_human.textContent = `You: ${humanChoice}`;
+  p_comp.textContent = `Computer: ${computerChoice}`;
+  result.appendChild(p_human);
+  results.appendChild(p_comp);
 
-  for (let i = 0; i < 5; i++) {
-    let comp = getComputerChoice();
-    let human = getHumanChoice();
-
-    console.log(`Round ${i + 1}`);
-    playRound(human, comp);
+  switch (result) {
+    case -2:
+      console.log("You win!");
+      humanScore += 1;
+      p_results.textContent = `You win!`;
+      p_score.textContent = `Current score: You - ${humanScore}       Computer - ${compScore}`
+      results.appendChild(p_results);
+      results.appendChild(p_score);
+      break;
+    case -1:
+      console.log("You lose...");
+      compScore += 1;
+      p_results.textContent = `You lose...`;
+      p_score.textContent = `Current score: You - ${humanScore}       Computer - ${compScore}`
+      results.appendChild(p_results);
+      results.appendChild(p_score);
+      break;
+    case 0:
+      console.log("It was a tie.");
+      p_results.textContent = `It was a tie.`;
+      p_score.textContent = `Current score: You - ${humanScore}       Computer - ${compScore}`
+      results.appendChild(p_results);
+      results.appendChild(p_score);
+      break;
+    case 1:
+      console.log("You win!");
+      humanScore += 1;
+      p_results.textContent = `You win!`;
+      p_score.textContent = `Current score: You - ${humanScore}       Computer - ${compScore}`
+      results.appendChild(p_results);
+      results.appendChild(p_score);
+      break;
+    case 2:
+      console.log("You lose...");
+      compScore += 1;
+      p_results.textContent = `You lose...`;
+      p_score.textContent = `Current score: You - ${humanScore}       Computer - ${compScore}`
+      results.appendChild(p_results);
+      results.appendChild(p_score);
+      break;
   }
-  console.log(`Final Score:\nYou: ${humanScore}\nComputer: ${compScore}`);
 
-  if (humanScore > compScore) {
-    console.log("You win!");
-  } else if (compScore > humanScore) {
-    console.log("You lose...");
-  } else {
-    console.log("It was a tie.");
+  if(humanScore === 5 || compScore === 5) {
+      if(humanScore > compScore){
+        p_final.textContent = `YOU WIN!`;
+        results.appendChild(p_final);
+      } else {
+        p_final.textContent = `YOU LOSE...`;
+        results.appendChild(p_final);
+      }
   }
 }
+
+let humanScore = 0;
+let compScore = 0;
+
+
+// for (let i = 0; i < 5; i++) {
+//   let comp = getComputerChoice();
+//   let human = getHumanChoice();
+
+//   console.log(`Round ${i + 1}`);
+//   playRound(human, comp);
+// }
+//console.log(`Final Score:\nYou: ${humanScore}\nComputer: ${compScore}`);
+
+if (humanScore > compScore) {
+  console.log("You win!");
+} else if (compScore > humanScore) {
+  console.log("You lose...");
+} else {
+  console.log("It was a tie.");
+}
+
+const results = document.querySelector(".results");
+const p_human =  document.createElement("p");
+const p_comp =  document.createElement("p");
+const p_results =  document.createElement("p");
+const p_score =  document.createElement("p");
+const p_final =  document.createElement("p");
+
+//set human choice to button clicked
+const btns = document.querySelectorAll("button");
+btns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    switch (btn.id) {
+      case "rock":
+        humanChoice = 1;
+        break;
+      case "paper":
+        humanChoice = 2;
+        break;
+      case "scissors":
+        humanChoice = 3;
+        break;
+    }
+    playRound(humanChoice, getComputerChoice());
+    console.log("Human Choice: " + humanChoice);
+    console.log(btn.id);
+  });
+});
